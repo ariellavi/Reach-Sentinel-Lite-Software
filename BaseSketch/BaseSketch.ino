7312d949e347d4dd6fce0bf317b1754784862d90
HirdayGupta <hirday.gupta@gmail.com>
2017-11-28 17:59:16 -0800

    Add datapacket struct

--- BaseSketch/BaseSketch.ino
+++ BaseSketch/BaseSketch.ino
@@ -23,6 +23,22 @@ Adafruit_MCP9808 tempsensor = Adafruit_MCP9808(); // Temperature Sensor
 */
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
+struct datapacket {
+  float accel_x;
+  float accel_y;
+  float accel_z;
+  float gyro_x;
+  float gyro_y;
+  float gyro_z;
+  float mag_x;
+  float max_y;
+  float mag_z;
+  float temp_tempF; // TODO: remove or keep depending on if the Adafruit_MCP9808 sensor is used
+  float baro_pascals;
+  float baro_altitude;
+  float baro_tempC;
+};
+
 void setup() {
   Serial.begin(9600);
 
